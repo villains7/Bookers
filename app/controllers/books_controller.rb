977b53e79dafabcc5fecfe4cfb3ba37541de0c65
EC2 Default User <ec2-user@ip-172-31-45-240.ec2.internal>
2022-01-08 14:27:57 +0000

    [Update ]220108 23:28

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,10 +5,14 @@ class BooksController < ApplicationController
   end
 
   def create
-   book = Book.new(book_params)
-   book.save
+   @book = Book.new(book_params)
+   if @book.save
+   redirect_to book_path(@book.id)
+   else
+    @books = Book.all
+   render :index
+   end
    flash[:create] ="Book was successfully created!"
-   redirect_to book_path(book.id)
   end
 
   def show
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,4 @@
 class Book < ApplicationRecord
+  validates :title, presence: {message: "Error"}
+  validates :body, presence: {message: "Error"}
 end
