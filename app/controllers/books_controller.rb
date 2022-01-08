e42b4e242ecd98595c93b6c55b07b81148d2f8f2
EC2 Default User <ec2-user@ip-172-31-45-240.ec2.internal>
2022-01-08 11:18:18 +0000

    [Update]220108 20:18

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,7 +7,8 @@ class BooksController < ApplicationController
   def create
    book = Book.new(book_params)
    book.save
-   redirect_to '/books'
+   flash[:create] ="Book was successfully created!"
+   redirect_to book_path(book.id)
   end
 
   def show
@@ -22,8 +23,17 @@ class BooksController < ApplicationController
   def update
   book = Book.find(params[:id])
   book.update(book_params)
+  flash[:update] = "Book was successfully update!"
   redirect_to book_path(book.id)
   end
+
+  def destroy
+  book = Book.find(params[:id])
+  book.destroy
+  redirect_to '/books'
+  flash[:delete] ="Book was successfully destroyed."
+  end
+
 
  private
   def book_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-root :to => 'homes#top'
-get 'books'=> 'books#index'
+root :to => 'homes#top'#トップページ
+get 'books'=> 'books#index' #一覧および新規投稿
 post 'books' => 'books#create'
-get 'books/:id' => 'books#show',as: 'book'
-get 'books/:id/edit' => 'books#edit',as: 'edit_book'
+get 'books/:id' => 'books#show',as: 'book'#詳細ページ
+get 'books/:id/edit' => 'books#edit',as: 'edit_book'#編集ページ
 patch 'books/:id' => 'books#update',as: 'update_book'
 delete 'books/:id' => 'books#destroy',as: 'destroy_book'
 end
