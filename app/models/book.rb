d502c7791ed4b9be0c0756ee4ad9877b03f33f54
EC2 Default User <ec2-user@ip-172-31-45-240.ec2.internal>
2022-01-09 06:09:02 +0000

    [Fix]テスト後だがテスト自体に問題あり

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,4 @@
 class Book < ApplicationRecord
-  validates :title, presence: {message: "Empty error"}
-  validates :body, presence: {message: "Empty error"}
+  validates :title, presence: true
+  validates :body, presence: true
 end
